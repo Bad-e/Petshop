64d9a60bec97ba1202735ece2ed3981a731fccee
Erick <ericksantosdasilva@gmail.com>
2025-09-27 15:52:26 -0300

    fix: tratando exception e corrigindo estrutura do menu

--- src/Petshop/Menu.java
+++ src/Petshop/Menu.java
@@ -1,16 +1,15 @@
 package Petshop;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
     static Scanner scanner = new Scanner(System.in);
     static PetMachine petMachine = new PetMachine();
 
-    //Criando o menu interativo
-
     public void menu() {
         scanner.useDelimiter("\\n");
-        int option;
+        int option = -1;
 
         do {
             System.out.println("\n==== Menu PetShop ====\n");
@@ -22,28 +21,41 @@ public class Menu {
             System.out.println("6 - verificar se tem pet no banho");
             System.out.println("7 - colocar pet na maquina");
             System.out.println("8 - retirar pet da máquina");
-            System.out.println("9 - limpar maquina\n");
+            System.out.println("9 - limpar maquina");
+            System.out.println("0 - Sair\n");
             System.out.print("Selecione uma opcao: ");
-            option = scanner.nextInt();
 
-            switch (option) {
-                case 1 -> petMachine.takeAShower();
-                case 2 -> petMachine.addWater();
-                case 3 -> petMachine.addShampoo();
-                case 4 -> petMachine.checkWater();
-                case 5 -> petMachine.checkShampoo();
-                case 6 -> petMachine.isPetInside();
-                case 7 -> insertPet();
-                case 8 -> petMachine.removePet();
-                case 9 -> petMachine.cleanMachine();
-                case 0 -> System.exit(0);
-                default -> System.out.println("Opção invalida");
+            try {
+                option = scanner.nextInt();
+
+                switch (option) {
+                    case 1 -> petMachine.takeAShower();
+                    case 2 -> petMachine.addWater();
+                    case 3 -> petMachine.addShampoo();
+                    case 4 -> petMachine.checkWater();
+                    case 5 -> petMachine.checkShampoo();
+                    case 6 -> petMachine.isPetInside();
+                    case 7 -> insertPet();
+                    case 8 -> petMachine.removePet();
+                    case 9 -> petMachine.cleanMachine();
+                    case 0 -> System.out.println("\nEncerrando o sistema.");
+                    default -> System.out.println("\nOpção invalida");
+                }
+            } catch (InputMismatchException exception){
+                System.out.println("\nEntrada inválida. Por favor, digite um número correspondente à opção.");
+                scanner.next();
+                option = -1;
             }
-        } while (true);
+        } while (option != 0);
     }
 
-    //Nomeia o pet e o coloca na máquina usando o método putPet
+    //Nomeia o pet e o coloca na máquina usando o metodo putPet
     public static void insertPet() {
+        if (petMachine.hasPet()){
+            System.out.println("\nUm pet já está dentro da máquina");
+            return;
+        }
+
         String name = "";
         Pet pet = new Pet(name);
 
